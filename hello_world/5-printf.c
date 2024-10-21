fbb06dd9c20665b53f52b15417b59059b3069fbb
chakir33 <mohamedchakirgnaoui@gmail.com>
2024-10-21 13:53:40 +0000

    tasc5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - main function
+ *
+ *Description: This function prints a phrase using printf.
+ *
+ *Return: Always returns 0 for success
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
