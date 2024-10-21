41c84eb729271b4ddb5d88ead5fc12d428b02eb8
chakir33 <mohamedchakirgnaoui@gmail.com>
2024-10-21 13:18:33 +0000

    tasc4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-
-int main() {
-    puts("Programming is like building a multilingual puzzle");
-    return 0;
+/**
+ *main - main function
+ *
+ *Description: This function prints a phrase using puts.
+ *
+ *Return: Always returns 0 for success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
