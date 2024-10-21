695ba1f82a8a7f1c5044866d98307da06a86e51a
chakir33 <mohamedchakirgnaoui@gmail.com>
2024-10-21 14:02:57 +0000

    tasc5

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,10 +2,11 @@
 /**
  *main - main function
  *
- *Description: This function prints a phrase using printf.
+ *Description: This function prints a phrase using puts.
  *
  *Return: Always returns 0 for success
  */
+
 int main(void)
 {
 	printf("\"with proper grammar, but the outcome is a piece of art,");
