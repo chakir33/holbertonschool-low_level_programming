320238006150d428ae6e6a18bf19a7343fc2dea1
chakir33 <mohamedchakirgnaoui@gmail.com>
2024-10-23 10:30:44 +0000

    tasc 4

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - dhhijdtyhjdt
+ *
+ *  Description: blalbla
+ *
+ * Return: return 0
+ **/
+
+	int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if ((c != 'e') && (c != 'q'))
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
