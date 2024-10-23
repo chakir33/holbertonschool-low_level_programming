8ddbd094a5abde1f25769912110014b159875d9b
chakir33 <mohamedchakirgnaoui@gmail.com>
2024-10-23 09:41:22 +0000

    tasc3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - dhhijdteihcieyhjdt
+ *
+ *  Description: blalbla
+ *
+ * Return: return 0
+ **/
+
+	int main(void)
+{
+	char c;
+	char C;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (C = 'A'; C <= 'Z'; c++)
+	{
+		putchar(C);
+	}
+	putchar('\n');
+	return (0);
+}
