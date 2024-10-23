4da18a69a6c4e36931a57f969831c6a7868d2b24
chakir33 <mohamedchakirgnaoui@gmail.com>
2024-10-23 10:53:50 +0000

    tasc5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - dhhijdtyhqsjdt
+ *
+ *  Description: blalbla
+ *
+ * Return: return 0
+ **/
+
+	int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
