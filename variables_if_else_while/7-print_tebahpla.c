10b1c8a6c94a5ad8a0a98ab0638a3af0d7703714
chakir33 <mohamedchakirgnaoui@gmail.com>
2024-10-23 12:15:23 +0000

    tasc 7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  maiin - ddhhijdtyhjdt
+ *
+ *  Description: blalbla
+ *
+ * Return: return 0
+ **/
+
+	int main(void)
+{
+	char c;
+
+	for (c = 'z'; c <= 'a'; c++)
+	{
+		if ((c != 'e') && (c != 'q'))
+			putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
