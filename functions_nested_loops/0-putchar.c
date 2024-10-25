c0614ea75a6aca05bceb1c6f6609b461d86010d9
chakir33 <mohamedchakirgnaoui@.com>
2024-10-25 09:40:12 +0000

    tasc0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ *  main - function
+ * Return: return 0
+ **/
+
+int main(void)
+{
+	{
+		putchar('_');
+		putchar('p');
+		putchar('u');
+		putchar('t');
+		putchar('c');
+		putchar('h');
+		putchar('a');
+		putchar('r');
+		putchar('\n');
+	}
+	return (0);
+}
