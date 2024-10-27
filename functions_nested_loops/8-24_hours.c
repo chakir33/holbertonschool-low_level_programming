b4038edcf28720360a21d3683de36375594fc581
chakir33 <mohamedchakirgnaoui@gmail.com>
2024-10-27 23:03:59 +0000

    tasc8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * main - return hours
+ *@c : 123
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int h = 0;
+	int m = 0;
+for (m = 0; m < 24; h++)
+{
+for (h = 0; h < 60; m++)
+}
+_putchar('0' + h / 10);
+_putchar('0' + h % 10);
+_putchar(':');
+_putchar('0' + m / 10);
+_putchar('0' + m % 10);
+_putchar('\n');
+}
