60a496bb1a950e69c407487c29ace2ca133894f5
chakir33 <mohamedchakirgnaoui@gmail.com>
2024-10-28 09:35:43 +0000

    tasc 10

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * add - operation of number
+ * @num1 : numero 1
+ * @num2 : numero 2
+ * Return: Always the reresult
+ * sum : its the result
+ */
+int add (int num1, int num2)
+{
+	int sum = num1 + num2;
+return (sum);
+
+}
