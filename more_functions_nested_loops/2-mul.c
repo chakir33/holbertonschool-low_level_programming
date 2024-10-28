7cb6f2593914aa0bbea58677daf635e734e85dfd
chakir33 <mohamedchakirgnaoui@gmail.com>
2024-10-28 11:06:40 +0000

    tasc2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - mult of number
+ * @a : numero 1
+ * @b : numero 2
+ * Return: Always the reresult
+ * sum : its the result
+ */
+int mul(int a, int b)
+{
+	int sum = a * b;
+return (sum);
+
+}
