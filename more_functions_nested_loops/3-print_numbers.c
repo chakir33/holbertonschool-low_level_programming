8d8bd52865662371c66eb276a43fcc4830a43b67
chakir33 <mohamedchakirgnaoui@gmail.com>
2024-10-28 11:52:13 +0000

    tasc3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -2,18 +2,15 @@
 
 /**
  * print_numbers - print 0 to 9
- *
- * Return: Always 0.
  */
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 48; i <= 57; c++)
-	{
+	for (i = 48; i <= 57; i++)
+{
 		_putchar(i);
-	}
+}
 	_putchar('\n');
-	return (0);
 }
