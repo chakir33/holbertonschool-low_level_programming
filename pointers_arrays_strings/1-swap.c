26d18e464754f724a7e47ffe400752f25c10990f
chakir33 <mohamedchakirgnaoui@gmail.com>
2024-10-29 16:03:04 +0000

    tasc1

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -8,18 +8,6 @@
  */
 void swap_int(int *a, int *b)
 {
-	int *p;
-	{
-	_putchar(a);
-	_putchar(b);
-	}
-	p = &a;
-	*p = 42;
-
-	 p = &b;
-	 *p = 98;
-	{
-		 _putchar(a);
-		 _putchar(b);
-	}
+	*a = 42;
+	*b = 98;
 }
