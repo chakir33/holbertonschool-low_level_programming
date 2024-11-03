b5d95ac5cb4abf36b69dc49ee38c4cfb97f05de5
chakir33 <mohamedchakirgnaoui@gmail.com>
2024-11-03 21:27:30 +0000

    tasc3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strcmp - code
+ *
+ * @s1: premier
+ * @s2: second
+ *
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	if (s1 == s2)
+	{
+		return (0);
+	}
+	return (0);
+}
