57a787210469db531adaf9eff274976291847b62
chakir33 <mohamedchakirgnaoui@gmail.com>
2024-11-05 11:06:26 +0000

    tasc8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_diagsums - calcul diagonale
+ *
+ * @a: array
+ * @size: size of the array
+ * Return: Always 0.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i = 0;
+	int result_1 = 0;
+	int result_2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		result_1 += a[i * size + 1];
+		result_2 += a[i * size + (size - 1 - i)];
+	}
+	printf("%i, %i\n", result_1, result_2);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,3 +1,4 @@
+void print_diagsums(int *a, int size);
 void print_chessboard(char (*a)[8]);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
